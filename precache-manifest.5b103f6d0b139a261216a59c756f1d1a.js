25cf69e7dbbca8b70c1a1648e5ed4eb86a561f1a
Nguyen Hoa Pham <nhpham27@gmail.com>
2022-07-10 15:46:14 -0500

    Updates

---
+++ precache-manifest.5b103f6d0b139a261216a59c756f1d1a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "30216d696314d9fec2c7",
+    "url": "/react-gh-pages/static/js/main.30216d69.chunk.js"
+  },
+  {
+    "revision": "a85c786083696c0e9ee2",
+    "url": "/react-gh-pages/static/js/1.a85c7860.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "0f4044ed203198c90303e567ef6e4180",
+    "url": "/react-gh-pages/index.html"
+  }
+];

63fd731651170f39de6b6c91fce6ae0936a88285
Nguyen Hoa Pham <nhpham27@gmail.com>
2022-07-10 18:19:12 -0500

    minor changes

---
+++ build/precache-manifest.f7ae29df24af90c2e3f01a399f56f96e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "30216d696314d9fec2c7",
+    "url": "/keeper-app/static/js/main.30216d69.chunk.js"
+  },
+  {
+    "revision": "a85c786083696c0e9ee2",
+    "url": "/keeper-app/static/js/1.a85c7860.chunk.js"
+  },
+  {
+    "revision": "6a6139f4d4003c4e2e4e",
+    "url": "/keeper-app/static/js/runtime~main.6a6139f4.js"
+  },
+  {
+    "revision": "e37b192485fa07a3efb761d5aac3d2bf",
+    "url": "/keeper-app/index.html"
+  }
+];
